ca6e249004dc6d4ff4d64cb71ee6222d343ab05b
smfl2022 <sophyfigaroa@gmail.com>
2026-03-12 22:29:36 -0700

    fixed 7 errors and finished 18

--- sql/07b.sql
+++ sql/07b.sql
@@ -6,13 +6,14 @@ SELECT DISTINCT title
 FROM film
 JOIN inventory USING (film_id)
 LEFT JOIN (
-    SELECT inventory_id
-    FROM rental
+    SELECT DISTINCT film_id
+    FROM inventory
+    JOIN rental USING (inventory_id)
     JOIN customer USING (customer_id)
     JOIN address USING (address_id)
     JOIN city USING (city_id)
     JOIN country USING (country_id)
     WHERE country = 'United States'
-) AS us_rentals USING (inventory_id)
-WHERE us_rentals.inventory_id IS NULL
+) AS us_films USING (film_id)
+WHERE us_films.film_id IS NULL
 ORDER BY title;
--- sql/18.sql
+++ sql/18.sql
@@ -7,3 +7,25 @@
  * The `to_char` function can be used to achieve the correct formatting of your percentage.
  * See: <https://www.postgresql.org/docs/current/functions-formatting.html#FUNCTIONS-FORMATTING-EXAMPLES-TABLE>
  */
+SELECT rank, title, revenue, "total revenue",
+       CASE
+           WHEN 100.0 * "total revenue" / sum(revenue) OVER () >= 100
+           THEN '100.00'
+           ELSE to_char(100.0 * "total revenue" / sum(revenue) OVER (), 'FM00.00')
+       END AS "percent revenue"
+FROM (
+    SELECT rank, title, film_id, revenue,
+           sum(revenue) OVER (ORDER BY rank) AS "total revenue"
+    FROM (
+        SELECT rank() OVER (ORDER BY revenue DESC) AS rank, title, film_id, revenue
+        FROM (
+            SELECT title, film_id, coalesce(sum(amount), 0.00) AS revenue
+            FROM film
+            LEFT JOIN inventory USING (film_id)
+            LEFT JOIN rental USING (inventory_id)
+            LEFT JOIN payment USING (rental_id)
+            GROUP BY title, film_id
+        ) AS t1
+    ) AS t2
+) AS t3
+ORDER BY rank, film_id;
